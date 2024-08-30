5baae2b093fce03844da87d54193b35404270e52
YersonValenzuela12 <yerson_1232@hotmail.com>
2024-08-30 07:39:06 -0500

    nuevos cambios en sesion 2 test1

--- sesion2/test1.py
+++ sesion2/test1.py
@@ -5,3 +5,7 @@ var2=20
 print("valor 1 es",var1)
 print("variable 2",var2)
 
+def sumar(var5, var6):
+ return var5 + var6
+resultado = sumar(var1, var2)
+print("el valor de la suma es".format(resultado))
