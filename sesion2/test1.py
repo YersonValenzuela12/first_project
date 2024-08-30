29b7db267667ae057f94156f605d16f2f0d6c614
YersonValenzuela12 <yerson_1232@hotmail.com>
2024-08-30 06:42:05 -0500

    nuevos cambios

---
+++ sesion2/test1.py
@@ -0,0 +1,7 @@
+
+"""variable"""
+var1=10
+var2=20
+print("valor 1 es",var1)
+print("variable 2",var2)
+
---
+++ sesion2/test2.py
@@ -0,0 +1,10 @@
+
+""""variables"""
+
+var3 = True
+var4 = False
+
+print("valor es de tipo",var3)
+
+complejo=1+2j
+print("la parte real del numero complejo es", complejo.real)
