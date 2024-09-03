3a64cc3c0ff19d7f154a170e35dc121f1535ce26
YersonValenzuela12 <yerson_1232@hotmail.com>
2024-09-02 21:37:28 -0500

    comenario clase 3

---
+++ sesion3/test1.py
@@ -0,0 +1,7 @@
+class carro:
+    """clase definida"""
+    ruedas = 6
+    def __init__(self, color, aceleracion):
+        self.color=color
+        self.aceleracion = aceleracion
+        self.velocidad = 0
